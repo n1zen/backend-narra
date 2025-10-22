92ed23684f09b64cea5817780a8c86ca1b519e15
n1zen <thisisnez1.2021@gmail.com>
2025-10-22 17:58:28 +0800

    commit uncommitted changes

---
+++ test_db.sql
@@ -0,0 +1,87 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.11.14-MariaDB, for debian-linux-gnu (aarch64)
+--
+-- Host: localhost    Database: test_db
+-- ------------------------------------------------------
+-- Server version	10.11.14-MariaDB-0+deb12u2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Parameters`
+--
+
+DROP TABLE IF EXISTS `Parameters`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Parameters` (
+  `Parameters_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Soil_ID` int(11) NOT NULL,
+  `HUM` float NOT NULL,
+  `TEMP` float NOT NULL,
+  `EC` float NOT NULL,
+  `PH` float NOT NULL,
+  `Comments` mediumtext NOT NULL,
+  `Date_Recorded` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`Parameters_ID`),
+  KEY `fk_Soil_ID` (`Soil_ID`),
+  CONSTRAINT `fk_Soil_ID` FOREIGN KEY (`Soil_ID`) REFERENCES `Soils` (`Soil_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Parameters`
+--
+
+LOCK TABLES `Parameters` WRITE;
+/*!40000 ALTER TABLE `Parameters` DISABLE KEYS */;
+INSERT INTO `Parameters` VALUES
+(1,1,25.6,29.3,215.32,6,'testing comment','2025-09-15 03:19:25');
+/*!40000 ALTER TABLE `Parameters` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Soils`
+--
+
+DROP TABLE IF EXISTS `Soils`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Soils` (
+  `Soil_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Soil_Name` varchar(255) NOT NULL,
+  `Soil_Location` point NOT NULL,
+  PRIMARY KEY (`Soil_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Soils`
+--
+
+LOCK TABLES `Soils` WRITE;
+/*!40000 ALTER TABLE `Soils` DISABLE KEYS */;
+INSERT INTO `Soils` VALUES
+(1,'testing','\æ\0\0\0\0\0L7‰A`…^À}\Ì¶B@');
+/*!40000 ALTER TABLE `Soils` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-15 11:24:08
